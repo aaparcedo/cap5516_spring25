44c08aa4e9d43558936c2113f70c9336ded027c9
aaparcedo <aaparcedo.io@gmail.com>
2025-02-09 15:31:50 -0500

    requirements

--- dataset_explore.py
+++ dataset_explore.py
@@ -3,7 +3,7 @@ import os
 
 # %%
 # DATASET_PATH = "/home/cap5516.student2/.cache/kagglehub/datasets/paultimothymooney/chest-xray-pneumonia/versions/2/chest_xray"
-DATASET_PATH = BALANCED_DATASET_PATH = "/home/cap5516.student2/cap5516_spring25/balanced_pneumonia"
+DATASET_PATH = BALANCED_DATASET_PATH = "~/cap5516_spring25/balanced_pneumonia"
 
 # How many NORMAL/PNEUMONIA in train/val/test?
 # %%
